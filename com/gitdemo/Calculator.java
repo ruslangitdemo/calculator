d6603bc58b4372a485cda4e1612f12a321de1c92
Ruslan 1 <ruslan-email-1@example.com>
2024-07-02 10:02:24 -0600

    cleanup

--- com/gitdemo/Calculator.java
+++ com/gitdemo/Calculator.java
@@ -3,11 +3,4 @@ package com.gitdemo;
 public class Calculator {
     public Calculator() {
     }
-
-    /**
-     * Adds two numbers
-     */
-    public double add(double a, double b) {
-        return a + b;
-    }
 }
