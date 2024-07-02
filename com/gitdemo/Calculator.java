5140c9ed59fbd068696fbb9abc7a0ed693fd8074
Ruslan 1 <ruslan-email-1@example.com>
2024-07-02 09:53:45 -0600

    Implement add (double version)

--- com/gitdemo/Calculator.java
+++ com/gitdemo/Calculator.java
@@ -3,4 +3,11 @@ package com.gitdemo;
 public class Calculator {
     public Calculator() {
     }
+
+    /**
+     * Adds two numbers
+     */
+    public double add(double a, double b) {
+        return a + b;
+    }
 }
